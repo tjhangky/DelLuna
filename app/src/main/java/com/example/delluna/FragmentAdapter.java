e5e1e3f34705f8c88e0931d74f226871c348973d
raynardtjhang <raytjhang@gmail.com>
2022-08-01 14:48:55 +0700

    up

---
+++ app/src/main/java/com/example/delluna/FragmentAdapter.java
@@ -0,0 +1,35 @@
+package com.example.delluna;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new Cloth();
+            case 1:
+                return new Album();
+            case 2:
+                return new Other();
+            default:
+                break;
+        }
+        return new Cloth();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/com/example/delluna/HomePage.java
@@ -0,0 +1,105 @@
+package com.example.delluna;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class HomePage extends AppCompatActivity {
+
+    TextView tvWelcome;
+    TabLayout tlTab;
+    ViewPager2 vpFragment;
+    Bundle extras;
+    FragmentAdapter fragmentAdapter;
+
+    void init() {
+        tvWelcome = findViewById(R.id.tv_welcome);
+        tlTab = findViewById(R.id.tl_tab);
+        vpFragment = findViewById(R.id.vp_fragment);
+        extras = getIntent().getExtras();
+        fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), getLifecycle());
+        vpFragment.setAdapter(fragmentAdapter);
+    }
+
+    void setUsername() {
+        String username = extras.getString("email");
+        tvWelcome.setText("welcome, " + username);
+    }
+
+//    Tabs
+    void populateTab() {
+        tlTab.addTab(tlTab.newTab().setText("Cloth"));
+        tlTab.addTab(tlTab.newTab().setText("Album"));
+        tlTab.addTab(tlTab.newTab().setText("Other"));
+    }
+
+    void setupTab() {
+        tlTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                vpFragment.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+//
+
+//    Sidebar Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case(R.id.i_logout): {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            }
+
+            case(R.id.i_about): {
+                Intent intent = new Intent(this, About.class);
+                startActivity(intent);
+                finish();
+                break;
+            }
+        }
+        return true;
+    }
+//
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        init();
+        setUsername();
+        populateTab();
+        setupTab();
+
+    }
+}
