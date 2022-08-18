1ccf2b09a5adb6fcb0230813a1d206f89e5be971
raynardtjhang <raytjhang@gmail.com>
2022-08-18 23:43:38 +0700

    login

--- app/src/main/java/com/example/delluna/MainActivity.java
+++ app/src/main/java/com/example/delluna/MainActivity.java
@@ -13,39 +13,39 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText etEmail, etPassword;
-    Button btnLogin, btnTest;
+    Button btnLogin;
 
     boolean validateEmail(String email){
-//        if(email.matches("")){
-//            Toast.makeText(this, "Email must be filled!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        } else if(!email.endsWith(".com")){
-//            Toast.makeText(this, "Email must end with '.com'!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        } else if(!email.contains("@")){
-//            Toast.makeText(this, "Email must contain '@'!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        } else if(email.contains(".@") || email.contains("@.") || email.contains(".@.")){
-//            Toast.makeText(this, "Email must not contain '@' and '.' adjacent each other!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        }
+        if(email.matches("")){
+            Toast.makeText(this, "Email must be filled!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(!email.endsWith(".com")){
+            Toast.makeText(this, "Email must end with '.com'!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(!email.contains("@")){
+            Toast.makeText(this, "Email must contain '@'!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(email.contains(".@") || email.contains("@.") || email.contains(".@.")){
+            Toast.makeText(this, "Email must not contain '@' and '.' adjacent each other!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
         return true;
     }
 
     boolean validatePassword(String password){
-//        if(password.matches("")){
-//            Toast.makeText(this, "Password must be filled!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        } else if(!password.matches("[A-Za-z0-9]*")){
-//            Toast.makeText(this, "Password must be alphanumeric, only alphabet and number allowed!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        } else if(!password.matches(".*[A-Za-z].*")){
-//            Toast.makeText(this, "Password must contain alphabet!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        } else if(!password.matches(".*[0-9].*")){
-//            Toast.makeText(this, "Password must contain number!", Toast.LENGTH_SHORT).show();
-//            return false;
-//        }
+        if(password.matches("")){
+            Toast.makeText(this, "Password must be filled!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(!password.matches("[A-Za-z0-9]*")){
+            Toast.makeText(this, "Password must be alphanumeric, only alphabet and number allowed!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(!password.matches(".*[A-Za-z].*")){
+            Toast.makeText(this, "Password must contain alphabet!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(!password.matches(".*[0-9].*")){
+            Toast.makeText(this, "Password must contain number!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
         return true;
     }
 
@@ -80,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
 
             if (validateEmail(email) && validatePassword(password)) {
                 Intent intent = new Intent(MainActivity.this, HomePage.class);
-//                intent.putExtra("username", email.substring(0, email.indexOf("@")));
-                intent.putExtra("username", email);
+                intent.putExtra("username", email.substring(0, email.indexOf("@")));
+//                intent.putExtra("username", email);
                 startActivity(intent);
                 finish();
             } else {
